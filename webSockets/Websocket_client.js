91a05d32d4249f886b5b564810534bed5cd48619
mohammedabboudi <mohammedabboudi10@gmail.com>
2022-03-29 14:39:07 +0000

    web socket sending form data from the client side to the server side

--- webSockets/Websocket_client.js
+++ webSockets/Websocket_client.js
@@ -1,1 +1,12 @@
 const WSC = new WebSocket('ws://localhost:4000');
+
+document.forms[0].onsubmit = ()=>{
+
+    let conversation = document.getElementById('conversation');
+    let message = document.getElementById('message');
+    console.log(message.value);
+    conversation.innerHTML += `<p> ${message.value}</p>`;
+    WSC.send(message.value);
+    message.value = '';
+
+}
--- webSockets/Websocket_server.js
+++ webSockets/Websocket_server.js
@@ -1,9 +1,17 @@
+const { json } = require('express/lib/response');
+
 const WebSocketServer = require('ws').Server;
 const WSS = new WebSocketServer({ port: 4000});
 
 WSS.on('connection', (ws)=>{
 
-    console.log('we are connected mohammed your using different port...')
+    console.log('we are connected mohammed using a different port...')
+
+    ws.on('message', (message) =>{
+
+      console.log(message.toString());
+
+    })
 
 })
 
